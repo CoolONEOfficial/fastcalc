7b63f152dbfa267000e8fc28b9c24eb3b4bf09af
cooloneofficial <coolone.official@gmail.com>
2017-10-25 22:43:00 +0300

    Added "..." for large numbers

--- app/src/main/java/ru/coolone/calculator/SplashActivity.java
+++ app/src/main/java/ru/coolone/calculator/SplashActivity.java
@@ -1,18 +1,20 @@
 package ru.coolone.calculator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
 
-        // Init splash activity
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
+		// Init splash activity
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+		finish();
+	}
 }
